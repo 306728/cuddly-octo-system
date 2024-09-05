5804f4a7beb1823fee4ca3f4fb0597c2d8415cc1
306728 <306728@gardenschool.edu.my>
2024-09-05 19:02:48 +0800

    Create AS13.py

---
+++ AS13/AS13.py
@@ -0,0 +1,5 @@
+s = float(input("Enter sales: "))
+gp = float(input("Enter gross profit: "))
+np = float(input("Enter net profit: "))
+
+print(f"Gross profit margin: {gp / s * 100:.2f}%\nNet profit margin: {np / s * 100:.2f}%")
