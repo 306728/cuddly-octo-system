58d27651fa03ad6636e91bc0cbc0d4468911c9b0
306728 <306728@gardenschool.edu.my>
2024-09-09 13:59:09 +0800

    Update AS15.py

--- AS/AS15/AS15.py
+++ AS/AS15/AS15.py
@@ -1,9 +1,23 @@
-edef f(n):
-    if n == 0: 
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return f(n - 1) + f(n - 2)
+import turtle
+import random
 
-print(f(int(input())))
+colours = ["AliceBlue", "AntiqueWhite", "aquamarine", "azure3", "bisque", "black", "BlanchedAlmond", "blue", "blue4", "brown", "brown3", "burlywood2", "CadetBlue", "chartreuse4", "chocolate2", "coral2", "CornflowerBlue", "cornsilk2", "cyan", "DarkBlue", "DarkGoldenrod", "firebrick", "green", "HotPink", "khaki", "MediumBlue", "navy", "purple", "tan", "yellow"]
+
+def draw_square(t, sz):
+    for i in range(4):
+        t.forward(sz)
+        t.left(90)
+
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+wn.title("Pretty patterns")
+
+turt = turtle.Turtle()
+
+turt.speed(0)
+
+for i in range(random.randint(30, 70)):
+    turt.pensize(random.randint(5, 10))
+    turt.color(random.choice(colours))
+    draw_square(turt, i * random.randint(3, 10))
+    turt.right(i)
