8fa782529a2c6c14c4575aadd7645dd62a927297
306728 <306728@gardenschool.edu.my>
2024-09-09 14:05:26 +0800

    Create AS17.py

---
+++ AS/AS17/AS17.py
@@ -0,0 +1,11 @@
+x = int(input())
+y = int(input())
+
+print(f"The statement x is greater than y is {x > y}")
+
+if x == y:
+    print("Equality has been achieved.")
+elif x > y:
+    print("x is greater than y.")
+else:
+    print("y is greater than x.")
