7d2a80f6889843e0faae191f136fbcd51365b6d8
306728 <306728@gardenschool.edu.my>
2024-09-10 20:31:38 +0800

    Create A259.py

---
+++ A2/A259/A259.py
@@ -0,0 +1,20 @@
+import math
+
+def f(x, y, n):
+    l = []
+    for i in range(n):
+        a = int(math.factorial(n)/(math.factorial(i) * math.factorial(n - i)))
+        if a == 1:
+            if i == 0:
+                l.append(f"({x})^{n} + ")
+            else:
+                l.append(f"({x})^{n - i}({y})^{i} + ")
+        else:
+            if i == 0:
+                l.append(f"{a}({x})^{n} + ")
+            else:
+                l.append(f"{a}({x})^{n - i}({y})^{i} + ")
+    l.append(f"({y})^{n}")
+    return "".join(l)
+
+print(f("x", "y", 4))
