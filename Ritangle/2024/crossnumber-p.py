82cd26a0aa4e308f2573a8c69307d5c8672795d1
306728 <306728@gardenschool.edu.my>
2024-11-28 08:31:21 +0000

    Updated Ritangle/2024/crossnumber-p.py

--- Ritangle/2024/crossnumber-p.py
+++ Ritangle/2024/crossnumber-p.py
@@ -13,7 +13,7 @@ def solve():
             for b in primes:
                 if 1 <= int(str(b)[1]) <= 4:
                     ac3 = str(b)[::-1]
-                    if (int(ac1[1]) + int(ac3[0])) ** (1/2) // 1 == (int(ac1[1]) + int(ac3[0])) ** (1/2):
+                    if ((int(ac1[1]) + int(ac3[0])) ** (1/2)).is_integer():
                         dn2 = int(f"{ac1[1]}{ac3[0]}")
                         for i in range(10):
                             if int(f"{ac3[1]}{i}") in triangle:
@@ -22,8 +22,8 @@ def solve():
                                     if str(dn4 * c)[0] == str(ac1)[0] and len(str(dn4 * c)) == 3 and int(str(dn4 * c)[2]) != 0:
                                         dn1 = int(dn4 * c)
                                         for j in range(1, 10):
-                                            if (sum(int(x) for x in str(ac1)) + sum(int(x) for x in str(ac3)) + int(str(dn4)[1]) + sum(int(x) for x in str(dn1)[1:]) + j) % 9 == 0:
+                                            if int(f"{str(dn1)[2]}{j}{str(dn4)[1]}") % (sum(int(x) for x in str(ac1)) + sum(int(x) for x in str(ac3)) + int(str(dn4)[1]) + sum(int(x) for x in str(dn1)[1:]) + j) == 0:
                                                 ac5 = int(f"{str(dn1)[2]}{j}{str(dn4)[1]}")
-                                                return ac1, ac3, dn2, dn4, dn1, ac5
+                                                return int(ac1), int(ac3), dn2, dn4, dn1, ac5
 
 print(solve())
